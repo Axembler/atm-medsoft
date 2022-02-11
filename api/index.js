ca6716abdfb035d25bfc7c8fc53007cef795f7cb
axembler <timur.payuk.axembler@mail.ru>
2022-02-11 15:53:07 +0300

    preparation

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ mongoose.connect(
   'mongodb://pagliaccio:89205103106@cluster0-shard-00-00.xzdxm.mongodb.net:27017,cluster0-shard-00-01.xzdxm.mongodb.net:27017,cluster0-shard-00-02.xzdxm.mongodb.net:27017/atm?ssl=true&replicaSet=atlas-qtyj9o-shard-0&authSource=admin&retryWrites=true&w=majority',
   { useNewUrlParser: true, useUnifiedTopology: true }
 )
-  .then(() => console.log('The connection to the MongoDB is established'))
+  .then(() => console.log('Connection to MongoDB is established'))
   .catch(err => console.error(err))
 
 const user = require('./routes/user')
@@ -23,16 +23,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(user)
 app.use(cors())
 
+const users = []
+const connections = []
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html')
+// })
+
+// ВЫВОД ПОЛЬЗОВАТЕЛЯ ПРИ ПОДКЛЮЧЕНИИ
+io.on('connection', (socket) => {
+  connections.push(socket)
+  console.log('User connected')
+
+  socket.send('Hello')
+
+  socket.on('disconnect', function(data) {
+    connections.splice(connections.indexOf(socket), 1)
+    console.log('User disconnected')
+  })
+})
+
+// СОЗДАНИЕ СЕРВЕРА С SOCKET.IO И ВЫВОД В КОНСОЛЬ
 if (require.main === module) {
   const port = process.env.PORT || 3001
 
   http.listen(port, () => {
-    console.log(`The server is running on ${port} port`)
+    console.log(`Server with socket.io is running on ${port} port`)
   })
 }
-
-io.on('connection', function() {
-  console.log('a user connected')
-})
 
 module.exports = app
