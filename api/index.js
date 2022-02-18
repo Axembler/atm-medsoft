323ca68321f6038d62e6f6b229cd3e736587f322
axembler <timur.payuk.axembler@mail.ru>
2022-02-18 17:21:16 +0300

    test

--- api/index.js
+++ api/index.js
@@ -10,6 +10,7 @@ const io = require('socket.io')(http, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
+    transports: ['websocket', 'polling'],
     credentials: true
   },
   allowEIO3: true
